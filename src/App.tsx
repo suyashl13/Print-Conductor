39dfcb48958898ce901a3ef30c9d04719f2c234f
Suyash Lawand (Mac) <suyash.lawand@gmail.com>
2023-05-17 17:35:31 +0530

    setup: integrated react in project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero vel hic modi minima nulla dolor suscipit omnis aliquid est, fugiat earum placeat consectetur molestiae obcaecati, asperiores assumenda reprehenderit eum culpa.
+    </div>
+  );
+}
